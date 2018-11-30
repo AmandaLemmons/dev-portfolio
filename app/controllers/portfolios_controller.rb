abaf1df73261be7d5e957cd4ade63362c4331af7
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-11-30 18:03:32 +0100

    add new and create to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,36 +1,30 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /portfolios
-  # GET /portfolios.json
-  def index
 
+  def index
     @portfolio_items = Portfolio.all
   end
 
-  # GET /portfolios/1
-  # GET /portfolios/1.json
+
   def show
   end
 
-  # GET /portfolios/new
   def new
     @portfolio_item = Portfolio.new
   end
 
-  # GET /portfolios/1/edit
   def edit
   end
 
-  # POST /portfolios
-  # POST /portfolios.json
+
   def create
     @portfolio_item = Portfolio.new(portfolio_item_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolio_item, notice: 'portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolios }
       else
         format.html { render :new }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
@@ -38,11 +32,10 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /portfolios/1
-  # PATCH/PUT /portfolios/1.json
+
   def update
     respond_to do |format|
-      if @portfolio_itenm.update(portfolio_itenm_params)
+      if @portfolio_item.update(portfolio_itenm_params)
         format.html { redirect_to @portfolio_itenm, notice: 'portfolio was successfully updated.' }
         format.json { render :show, status: :ok, location: @portfolio_itenm }
       else
@@ -52,8 +45,7 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  # DELETE /portfolios/1
-  # DELETE /portfolios/1.json
+
   def destroy
     @portfolio_item.destroy
     respond_to do |format|
@@ -63,13 +55,11 @@ class PortfoliosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_portfolio_item
-      @portfolio_item = portfolio.find(params[:id])
+      @portfolio_item = Portfolio.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_item_params
-      params.require(:portfolio).permit(:title, :body)
+      params.require(:portfolio).permit(:title, :body, :subtitle, :thumb_image)
     end
 end
