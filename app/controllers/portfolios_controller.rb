e746de6010e50b9f04310392e493c97aadb6c533
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-11-30 17:15:16 +0100

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,75 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
+  # GET /portfolios
+  # GET /portfolios.json
+  def index
+
+    @portfolio_items = Portfolio.all
+  end
+
+  # GET /portfolios/1
+  # GET /portfolios/1.json
+  def show
+  end
+
+  # GET /portfolios/new
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  # GET /portfolios/1/edit
+  def edit
+  end
+
+  # POST /portfolios
+  # POST /portfolios.json
+  def create
+    @portfolio_item = Portfolio.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to @portfolio_item, notice: 'portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /portfolios/1
+  # PATCH/PUT /portfolios/1.json
+  def update
+    respond_to do |format|
+      if @portfolio_itenm.update(portfolio_itenm_params)
+        format.html { redirect_to @portfolio_itenm, notice: 'portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_itenm }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_itenm.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /portfolios/1
+  # DELETE /portfolios/1.json
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'portfolio was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio_item
+      @portfolio_item = portfolio.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_item_params
+      params.require(:portfolio).permit(:title, :body)
+    end
 end
