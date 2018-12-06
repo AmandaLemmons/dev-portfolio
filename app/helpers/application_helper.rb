3ef133111416f190054608960561b6d69dae5b1b
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-12-06 13:27:01 +0100

    added jquery to js files to fix logout bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       "<br>".html_safe +
       (link_to 'Login', new_user_session_path)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      (link_to 'Logout', destroy_user_session_path, method: :delete)
      end
   end
 
