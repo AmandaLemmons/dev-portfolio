fcd81e4ec8966c1ed36013a37b2158fe98421ae0
Amanda Lemmons <amanda.lemmons@gmail.com>
2018-12-10 18:19:00 +0100

    upload and photos working

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -5,13 +5,12 @@ module PortfoliosHelper
   end
 
   def portfolio_img img, type
-    if img
-      img
-    elsif type == 'thumb'
-      image_generator(height: '350', width: '200')
-    else
-      image_generator(height: '600', width: '400')
+      if img
+        img
+      elsif type == 'thumb'
+        image_generator(height: '350', width: '200')
+      elsif type == 'main'
+        image_generator(height: '600', width: '400')
+      end
     end
-  end
-
 end
