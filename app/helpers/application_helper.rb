72144c7188143bbd0f6aee32809d23bd4b9bd235
Amanda Lemmons <amanda.lemmons@gmail.com>
2019-04-26 14:03:04 +0200

    updated copyright text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    CopyRightViewTool::Renderer.copyright('Amanda Lemmons', '| Some rights reserved')
+    CopyRightViewTool::Renderer.copyright('Amanda Lemmons', '| Poco rights reserved')
   end
 
   def nav_items
